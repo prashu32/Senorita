f8a5cd5a629dc4a3081d8adefe35e2faec217380
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-27 19:13:28 +0530

    Update welcome.py

--- senorita/modules/welcome.py
+++ senorita/modules/welcome.py
@@ -139,7 +139,7 @@ def new_member(bot: Bot, update: Update):
         for new_mem in new_members:
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_text("Oh🤴Genos,My Owner has just joined your group.")
+                update.effective_message.reply_text("Oh🤴Genos,My Owner My love❤️ has just joined your group.")
                 continue
             
             # Welcome Devs
@@ -159,7 +159,7 @@ def new_member(bot: Bot, update: Update):
                 update.effective_message.reply_text("Oof! A Whitelist User just joined!")
 
             elif new_mem.id ==  1137511834:
-                update.effective_message.reply_text("Whoa! My Creator/Developer has just joined your group. Happy to have Mystery here.")
+                update.effective_message.reply_text("Whoa! My Creator/Developer My lover❤️ has just joined your group. Happy to have Mystery here.")
 
             # Make bot greet admins
             elif new_mem.id == bot.id:
