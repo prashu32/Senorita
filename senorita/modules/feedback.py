29add18f6a6a89bc41fca3e64d31cce9c301fdf5
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-28 00:48:52 +0530

    Update feedback.py

--- senorita/modules/feedback.py
+++ senorita/modules/feedback.py
@@ -19,7 +19,7 @@ def feedback(bot: Bot, update: Update):
   feed_text = f"senorita's *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
   
 
-  bot.send_message(--1001349150416, feed_text, parse_mode=ParseMode.MARKDOWN)
+  bot.send_message(-1001349150416, feed_text, parse_mode=ParseMode.MARKDOWN)
  
   text = html.escape(text)
   reply_text=f"Thankyou for giving us your feedback."
