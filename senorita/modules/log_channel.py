5813bbe897dc8b0282f5d1fd8c43c5a25a9dc8cb
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-27 14:33:11 +0530

    Update log_channel.py

--- senorita/modules/log_channel.py
+++ senorita/modules/log_channel.py
@@ -11,9 +11,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from miley import dispatcher, LOGGER, GBAN_LOGS
-    from miley.modules.helper_funcs.chat_status import user_admin
-    from miley.modules.sql import log_channel_sql as sql
+    from senorita import dispatcher, LOGGER, GBAN_LOGS
+    from senorita.modules.helper_funcs.chat_status import user_admin
+    from senorita.modules.sql import log_channel_sql as sql
 
 
     def loggable(func):
