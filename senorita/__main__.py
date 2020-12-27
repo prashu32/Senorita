41760137b8b69254e52973c151aeb2115f3b77aa
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-27 21:01:02 +0530

    Update __main__.py

--- senorita/__main__.py
+++ senorita/__main__.py
@@ -28,11 +28,11 @@ from senorita.modules.connection import connect_button
 
 PM_START_TEXT = """
 _Hello_ *{}*
-_My name is_ *{}*\n A Powerful Telegram Bot to Manage Your Groups,feel free to add to your groups!!
+_My name is_ *{}*\n A PØWERFUL RØMANTIC TELEGRÃM BØT TØ MÃNÃGE YØÚR GRŌÚPS,FÈÈL FRËË TØ ÃDD MÈ TØ YØUR GRØUPS!!
 
 Support Channel : *@SPAMREPORTER*
 
-_I'm maintained by My lover_ [{}](tg://user?id={})
+_I'M MAINTÅINËD BY MY lØVER❤️_ [{}](tg://user?id={})
  
 """
 
@@ -42,11 +42,11 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
-🔥 - /start: start the bot
-🔥 - /help: PM's you this message.
-🔥 - /help <module name>: PM's you info about that module.
-🔥 - /source: Information about my source.
-🔥 - /settings:
+❤️ - /start: start the bot
+❤️ - /help: PM's you this message.
+❤️ - /help <module name>: PM's you info about that module.
+❤️ - /source: Information about my source.
+❤️ - /settings:
    🔹 - in PM: will send you your settings for all supported modules.
    🔹 - in a group: will redirect you to pm, with all that chat's settings.
 {}
@@ -184,7 +184,7 @@ def send_start(bot, update):
     text = PM_START_TEXT
 
     keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="❤️Creator❤️",url="https://t.me/Mbbs_lover")]]
-    keyboard += [[InlineKeyboardButton(text="🔥🌹Connect Group🌹🔥", callback_data="main_connect"),InlineKeyboardButton(text="💐 Add Me 💐",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard += [[InlineKeyboardButton(text="Connect Group🌹", callback_data="main_connect"),InlineKeyboardButton(text="💐 Add Me 💐",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
