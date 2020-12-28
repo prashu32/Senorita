5c1af7cb1b55af6ec6ceb6868c2e4119fea48916
PROFESSOR <75575809+prashu32@users.noreply.github.com>
2020-12-28 10:25:09 +0530

    Update global_bans.py

--- senorita/modules/global_bans.py
+++ senorita/modules/global_bans.py
@@ -113,7 +113,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
 
         return
     
-    message.reply_text("⚡️ *Snaps the Banhammer* ⚡️")
+    message.reply_text("⚡️ *CHAL BHAGG BOCHLIKEE* ⚡️")
     
     start_time = time.time()
     datetime_fmt = "%H:%M - %d-%m-%Y"
